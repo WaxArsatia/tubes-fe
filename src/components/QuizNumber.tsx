fa31bb8c4401afd9f9ec97995a1acc93af144709
WaxArsatia <waxarsatia@gmail.com>
2025-11-27 17:57:49 +0700

    feat: Refactor & Add State for Quiz Page
    Signed-off-by: WaxArsatia <waxarsatia@gmail.com>

--- src/components/QuizNumber.tsx
+++
@@ -1,5 +0,0 @@
-export default function QuizNumber({number = ''}) {
-  return (
-    <div className="w-12 h-12 pt-3 mx-auto mb-3 text-center rounded-md cursor-pointer lg:pt-5 bg-neutral-300 lg:w-16 lg:h-16">{number}</div>
-  )
-}
