b57dceebd2038d7ab3b68012214283a8ba9f3de3
WaxArsatia <waxarsatia@gmail.com>
2025-12-21 21:00:39 +0700

    feat: Clean and setup for redesign
    Signed-off-by: WaxArsatia <waxarsatia@gmail.com>

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,18 +1,9 @@
 import { createFileRoute } from "@tanstack/react-router";
-import Dashboard from "@/components/Dashboard";
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
 
 export const Route = createFileRoute("/")({
   component: App,
 });
 
 function App() {
-  return (
-    <>
-      <Header />
-      <Dashboard />
-      <Footer />
-    </>
-  );
+  return <div>Hello World</div>;
 }
